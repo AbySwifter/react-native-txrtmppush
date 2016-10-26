02da17f05da9a17329af962360ae5f09d1c1dc86
Bear.Wang <wyx9653@163.com>
2016-10-26 17:28:15 +0800

    Initial Commit

---
+++ react-native-txrtmppush/react_native_txrtmppush.h
@@ -0,0 +1,13 @@
+//
+//  react_native_txrtmppush.h
+//  react-native-txrtmppush
+//
+//  Created by Bear on 2016/10/26.
+//  Copyright © 2016年 dragontrail. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface react_native_txrtmppush : NSObject
+
+@end
---
+++ react-native-txrtmppush/react_native_txrtmppush.m
@@ -0,0 +1,13 @@
+//
+//  react_native_txrtmppush.m
+//  react-native-txrtmppush
+//
+//  Created by Bear on 2016/10/26.
+//  Copyright © 2016年 dragontrail. All rights reserved.
+//
+
+#import "react_native_txrtmppush.h"
+
+@implementation react_native_txrtmppush
+
+@end
